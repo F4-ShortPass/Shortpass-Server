bf740f3afd60ac60bd291a0a4441f60eeeb406e5
rosaze <geenieeyoon@gmail.com>
2025-10-22 08:20:00 +0900

    feat: store persona profiles and visualization data

---
+++ server/models/__init__.py
@@ -0,0 +1,16 @@
+# server/models/__init__.py
+from .job import Job, JobChunk
+from .interview import InterviewSession, InterviewResult, Question, InterviewStatus, Company, Applicant, PersonaDB, ResumeChunk
+
+__all__ = [
+    "Job",
+    "JobChunk",
+    "InterviewSession",
+    "InterviewResult",
+    "Question",
+    "InterviewStatus",
+    "Company",
+    "Applicant",
+    "PersonaDB",
+    "ResumeChunk"
+]
